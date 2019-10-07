432a06de29989f048d2190647eff7a73867c84af
xuchen <monkeydpotato@gmail.com>
2019-10-07 11:41:17 +0800

    dms-sdk should not be config by root project, because it would be refer by dm-base

--- dms-core/build.gradle.kts
+++ dms-core/build.gradle.kts
@@ -1,3 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // spring
+    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    // kotlin
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
+}
+
+group = "com.monkeydp.daios.dms"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
 val developmentOnly by configurations.creating
 configurations {
     runtimeClasspath {
@@ -9,10 +23,22 @@ configurations {
 }
 
 dependencies {
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // monkeydp
+    implementation("com.monkeydp:tools")
     // spring
     implementation("org.springframework.boot:spring-boot-starter")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     // other modules
     implementation(project(":dms-sdk"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
--- dms-sdk/build.gradle.kts
+++ dms-sdk/build.gradle.kts
@@ -1,3 +1,30 @@
-dependencies{
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // spring
+    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    // kotlin
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
+}
+
+group = "com.monkeydp.daios.dms"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+dependencies {
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // monkeydp
+    implementation("com.monkeydp:tools")
+    // junit
     testImplementation("junit:junit")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
