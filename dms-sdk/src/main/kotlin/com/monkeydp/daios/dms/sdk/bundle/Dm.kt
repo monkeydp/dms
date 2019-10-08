458265d73fdc145f0b427aa0cdb4b00b0ec41ff8
xuchen <monkeydpotato@gmail.com>
2019-10-08 15:26:32 +0800

    temp

---
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/bundle/Dm.kt
@@ -0,0 +1,19 @@
+package com.monkeydp.daios.dms.sdk.bundle
+
+/**
+ * Datasource module
+ *
+ * @author iPotato
+ * @date 2019/10/8
+ */
+interface Dm {
+
+    fun implClassNames(): ImplClassNames
+
+    /**
+     * Implementation class names of all sdk contract
+     */
+    interface ImplClassNames {
+        fun connectionFactory(): String?
+    }
+}
