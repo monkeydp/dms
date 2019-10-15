71ca3da74821d454460ba9d9dba68de741ab2bef
xuchen <monkeydpotato@gmail.com>
2019-10-15 13:47:18 +0800

    init dm impls

--- dms-core/src/main/kotlin/com/monkeydp/daios/dms/bundle/DmBundle.kt
+++ dms-core/src/main/kotlin/com/monkeydp/daios/dms/bundle/DmBundle.kt
@@ -1,5 +1,6 @@
 package com.monkeydp.daios.dms.bundle
 
+import com.monkeydp.daios.dms.sdk.connection.ConnectionFactory
 import com.monkeydp.daios.dms.sdk.dm.Dm
 import com.monkeydp.tools.util.ClassUtil
 import java.io.File
@@ -18,10 +19,16 @@ class DmBundle(private val deployDir: File, private val dmClassname: String) {
 
     private val classLoader: BundleClassLoader
     private val dm: Dm
+    private val impls = Impls()
+
+    private class Impls {
+        lateinit var connectionFactory: ConnectionFactory
+    }
 
     init {
         classLoader = initBundleClassLoader()
         dm = initDm()
+        initAllImplClasses()
     }
 
     private fun initBundleClassLoader(): BundleClassLoader {
@@ -33,6 +40,13 @@ class DmBundle(private val deployDir: File, private val dmClassname: String) {
         @Suppress("UNCHECKED_CAST")
         val dmClass: Class<out Dm> = classLoader.loadClass(dmClassname) as Class<out Dm>
         return ClassUtil.newInstance(dmClass)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun initAllImplClasses() {
+        val implClassNames = dm.implClassNames()
+        val connectionFactoryClass = classLoader.loadClass(implClassNames.connectionFactory()) as Class<ConnectionFactory>
+        impls.connectionFactory = ClassUtil.newInstance(connectionFactoryClass)
     }
 
     private class BundleClassLoader(urls: Array<URL>, parent: ClassLoader) : URLClassLoader(urls, parent) {
