e8e4e8b259b0f07ce8308bda7149b43fc1f0c786
iPotato <monkeydpotato@gmail.com>
2019-10-24 21:55:43 +0800

    *

--- dms-core/src/main/kotlin/com/monkeydp/daios/dms/connection/ConnectionManager.kt
+++ dms-core/src/main/kotlin/com/monkeydp/daios/dms/connection/ConnectionManager.kt
@@ -28,34 +28,56 @@ class ConnectionManager {
         return this
     }
     
-    @Synchronized
     fun activateConn(cw: ConnectionWrapper): ConnectionManager {
         val cpw = activeCpwMap[cw.cpId]!!
-        if (cw.belongsToUser() && cpw.hasActiveUserConn) ierror("Active user connection is already exist!")
-        cpw.activeCwMap.putIfAbsent(cw.connId, cw)
+        cpw.activateConn(cw)
         return this
     }
     
     fun getActiveCp(cpId: Long) = activeCpwMap[cpId]!!.cp
     
-    fun getActiveCw(cpId: Long, connId: Long) = activeCpwMap[cpId]!!.activeCwMap[connId]!!
+    fun getActiveCw(cpId: Long, connId: Long) = activeCpwMap[cpId]!!.getActiveCw(connId)
     
     fun getActiveUserCw(cpId: Long): ConnectionWrapper = activeCpwMap[cpId]!!.getActiveUserCw()
     
     private class ConnectionProfileWrapper(val cp: ConnectionProfile) {
         
         val cpId: Long = cp.id
-        var hasActiveUserConn = false
-        var activeUserConnId = -1L
+    
+        private companion object {
+            const val INVALID_CONN_ID = -1L
+        }
+    
+        private var hasActiveUserConn = false
+        private var activeUserConnId = INVALID_CONN_ID
+            set(value) {
+                hasActiveUserConn = value != INVALID_CONN_ID
+                field = value
+            }
         
         /**
          * @see ConnectionWrapper.connId
          */
-        val activeCwMap = mutableMapOf<Long, ConnectionWrapper>()
+        private val activeCwMap = mutableMapOf<Long, ConnectionWrapper>()
+    
+        @Synchronized
+        fun activateConn(cw: ConnectionWrapper) {
+            if (cw.belongsToUser()) {
+                if (hasActiveUserConn) ierror("Active user connection is already exist!")
+                activeUserConnId = cw.connId
+            }
+            activeCwMap.putIfAbsent(cw.connId, cw)
+        }
+    
+        fun getActiveCw(connId: Long): ConnectionWrapper {
+            val activeCw = activeCwMap[connId]
+            if (activeCw == null) ierror("The active connection corresponding to id ${connId} does not exist")
+            return activeCwMap[connId]!!
+        }
         
         fun getActiveUserCw(): ConnectionWrapper {
             if (!hasActiveUserConn) ierror("Active user connection is not exist!")
-            return activeCwMap[activeUserConnId]!!
+            return getActiveCw(activeUserConnId)
         }
     }
 }
