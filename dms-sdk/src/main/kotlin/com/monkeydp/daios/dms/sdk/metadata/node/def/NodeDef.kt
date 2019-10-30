6affdb2a1ad9d61ccd6ce3fb611ba4d8e26c2e7d
iPotato <monkeydpotato@gmail.com>
2019-10-30 16:37:25 +0800

    add structure name to node def

--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/node/def/NodeDef.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/node/def/NodeDef.kt
@@ -7,6 +7,7 @@ import com.monkeydp.daios.dms.sdk.metadata.node.NodeInfo
  * @date 2019/10/25
  */
 interface NodeDef {
+    val structureName: String
     val info: NodeInfo
     val parent: NodeDef?
     val children: List<NodeDef>
--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/node/def/impl/AbstractNd.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/node/def/impl/AbstractNd.kt
@@ -1,12 +1,20 @@
 package com.monkeydp.daios.dms.sdk.metadata.node.def.impl
 
 import com.monkeydp.daios.dms.sdk.metadata.node.def.NodeDef
+import com.monkeydp.tools.ext.camelCase2List
+import com.monkeydp.tools.ext.lastOf
 
 /**
  * @author iPotato
  * @date 2019/10/27
  */
 abstract class AbstractNd : NodeDef {
+    override val structureName: String
+        get() {
+            val classname = this.javaClass.simpleName
+            if (classname == "abstract") return "<no name for abstract>"
+            return classname.camelCase2List().lastOf(1).toLowerCase()
+        }
     override val parent: NodeDef? = null
     override val children = emptyList<NodeDef>()
 }
