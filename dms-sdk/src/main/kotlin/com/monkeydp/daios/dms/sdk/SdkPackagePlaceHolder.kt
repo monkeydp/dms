f36b17e2c2b2425deef818469140f22641468634
iPotato <monkeydpotato@gmail.com>
2019-11-09 22:06:58 +0800

    temp

--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/SdkPackagePlaceHolder.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/SdkPackagePlaceHolder.kt
@@ -1,9 +1,7 @@
 package com.monkeydp.daios.dms.sdk
 
-import com.monkeydp.daios.dms.sdk.useful.PackagePlaceHolder
-
 /**
  * @author iPotato
  * @date 2019/11/8
  */
-object SdkPackagePlaceHolder : PackagePlaceHolder
+object SdkPackagePlaceHolder
--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/useful/PackagePlaceHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dms.sdk.useful
-
-/**
- * Package name of impl class would be used
- * @author iPotato
- * @date 2019/11/9
- */
-interface PackagePlaceHolder {
-}
