54c83059912cc3143cdb64057717e95379033ccf
iPotato <monkeydpotato@gmail.com>
2019-11-10 19:37:32 +0800

    *

--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/form/FormLoadingCtx.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/form/FormLoadingCtx.kt
@@ -1,15 +1,19 @@
 package com.monkeydp.daios.dms.sdk.metadata.form
 
+import com.monkeydp.daios.dms.sdk.annot.NeedDatasource
 import com.monkeydp.daios.dms.sdk.helper.IdHelper
 import com.monkeydp.daios.dms.sdk.instruction.Instruction
 import com.monkeydp.daios.dms.sdk.mocker.ConnJsonMocker
 import com.monkeydp.daios.dms.sdk.mocker.InstrJsonMocker
+import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 /**
  * @author iPotato
  * @date 2019/11/8
  */
+@ApiModel
+@NeedDatasource
 class FormLoadingCtx(
         @ApiModelProperty(required = true, example = ConnJsonMocker.CP_ID)
         val cpId: Long = IdHelper.INVALID_ID,
