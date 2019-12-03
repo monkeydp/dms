fb3d7ab1aa067f17a64aa2f131376128dce069a6
iPotato <monkeydpotato@gmail.com>
2019-12-03 22:28:31 +0800

    opt

--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/icon/GlobalIcon.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/metadata/icon/GlobalIcon.kt
@@ -18,5 +18,11 @@ enum class GlobalIcon(
     GROUP_ICON("default group icon", ""),
     DB_GROUP_ICON("db group icon", ""),
     TABLE_GROUP_ICON("table group icon", ""),
-    VIEW_GROUP_ICON("view group icon", ""),
+    VIEW_GROUP_ICON("view group icon", "");
+    
+    companion object {
+        const val SUFFIX = "_ICON"
+        const val GROUP_SUFFIX = "_GROUP$SUFFIX"
+        fun appendSuffix(str: String, suffix: String = SUFFIX) = "$str$suffix"
+    }
 }
