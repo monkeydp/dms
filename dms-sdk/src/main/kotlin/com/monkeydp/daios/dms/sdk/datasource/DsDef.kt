0a3995cb4ab312e83c769cf5b6af0db03522b128
iPotato <monkeydpotato@gmail.com>
2019-12-23 21:59:02 +0800

    *

--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/datasource/DsDef.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/datasource/DsDef.kt
@@ -4,5 +4,13 @@ package com.monkeydp.daios.dms.sdk.datasource
  * Datasource definition
  */
 interface DsDef {
-    var version: DsVersion<*>
+    val version: DsVersion<*>
+    
+    companion object {
+        operator fun invoke(version: DsVersion<*>): DsDef = StdDsDef(version)
+    }
 }
+
+abstract class AbstractDsDef(override val version: DsVersion<*>) : DsDef
+
+private class StdDsDef(version: DsVersion<*>) : AbstractDsDef(version)
