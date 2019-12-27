461ae959df719f253e069d1b9ad66582cc3411bf
iPotato <monkeydpotato@gmail.com>
2019-12-27 14:25:52 +0800

    add MongodbConnApi

---
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/instruction/main/CloseConn.kt
@@ -0,0 +1,9 @@
+package com.monkeydp.daios.dms.sdk.instruction.main
+
+import com.monkeydp.daios.dms.sdk.instruction.AbstractInstr
+
+/**
+ * @author iPotato
+ * @date 2019/10/31
+ */
+object CloseConn : AbstractInstr()
--- dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/received/form/annot/SdkNewConnForm.kt
+++ dms-sdk/src/main/kotlin/com/monkeydp/daios/dms/sdk/received/form/annot/SdkNewConnForm.kt
@@ -1,13 +1,12 @@
 package com.monkeydp.daios.dms.sdk.received.form.annot
 
 import com.monkeydp.daios.dms.sdk.conn.NewConnForm
-import com.monkeydp.daios.dms.sdk.instruction.Instruction
+import com.monkeydp.daios.dms.sdk.instruction.main.NewConn
 import com.monkeydp.daios.dms.sdk.received.form.ReceivedForm
 import com.monkeydp.tools.ext.kodein.component.AbstractKodeinBuilderConfig
 import com.monkeydp.tools.ext.kodein.component.KodeinComponent
 import com.monkeydp.tools.ext.kodein.component.KodeinKClassComp
 import com.monkeydp.tools.ext.kodein.singletonX
-import com.monkeydp.tools.ext.kotlin.singletonX
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import kotlin.annotation.AnnotationTarget.CLASS
@@ -19,12 +18,11 @@ import kotlin.reflect.KClass
  */
 @Target(CLASS)
 @KodeinComponent(SdkNewConnForm.KodeinBuilderConfig::class)
-annotation class SdkNewConnForm(val instrKClass: KClass<out Instruction>) {
+annotation class SdkNewConnForm {
     object KodeinBuilderConfig : AbstractKodeinBuilderConfig<KodeinKClassComp>() {
         override fun Kodein.Builder.configOne(comp: KodeinKClassComp) {
             bind<KClass<out NewConnForm>>() with singletonX { comp.annotatedKClass }
-            val instr = (comp.annot as SdkNewConnForm).instrKClass.singletonX()
-            bind<KClass<out ReceivedForm>>(tag = instr) with singletonX { comp.annotatedKClass }
+            bind<KClass<out ReceivedForm>>(tag = NewConn) with singletonX { comp.annotatedKClass }
         }
     }
 }
